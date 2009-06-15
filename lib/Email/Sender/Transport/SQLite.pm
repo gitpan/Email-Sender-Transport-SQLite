a60f921cbc6e2c5b54216cd0c434d7d4025a2560
Ricardo SIGNES <rjbs@cpan.org>
2009-06-15 23:04:40 +0000

    Import of RJBS/Email-Sender-Transport-SQLite-0.091660 from CPAN.
    gitpan-cpan-distribution: Email-Sender-Transport-SQLite
    gitpan-cpan-version:      0.091660
    gitpan-cpan-path:         RJBS/Email-Sender-Transport-SQLite-0.091660.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/Sender/Transport/SQLite.pm
+++ lib/Email/Sender/Transport/SQLite.pm
@@ -1,14 +1,10 @@
 package Email::Sender::Transport::SQLite;
+our $VERSION = '0.091660';
+
 use Moose;
-extends 'Email::Sender::Transport';
+with 'Email::Sender::Transport';
+# ABSTRACT: deliver mail to an sqlite db for testing
 
-our $VERSION = '0.002';
-
-=head1 NAME
-
-Email::Sender::Transport::SQLite - deliver mail to an sqlite db for testing
-
-=cut
 
 use DBI;
 
@@ -49,6 +45,7 @@ has db_file => (
 sub _setup_dbh {
   my ($self) = @_;
   my $dbh = $self->_dbh;
+
   $dbh->do('
     CREATE TABLE emails (
       id INTEGER PRIMARY KEY,
@@ -56,6 +53,7 @@ sub _setup_dbh {
       env_from varchar NOT NULL
     );
   ');
+
   $dbh->do('
     CREATE TABLE recipients (
       id INTEGER PRIMARY KEY,
@@ -74,14 +72,22 @@ sub send_email {
 
   my $dbh = $self->dbh;
 
-  $dbh->do("INSERT INTO emails (body, env_from) VALUES (?, ?)",
-    undef, $message, $from,);
+  $dbh->do(
+    "INSERT INTO emails (body, env_from) VALUES (?, ?)",
+    undef,
+    $message,
+    $from,
+  );
 
   my $id = $dbh->last_insert_id((undef) x 4);
 
   for my $addr (@$to) {
-    $dbh->do("INSERT INTO recipients (email_id, env_to) VALUES (?, ?)",
-      undef, $id, $addr,);
+    $dbh->do(
+      "INSERT INTO recipients (email_id, env_to) VALUES (?, ?)",
+      undef,
+      $id,
+      $addr,
+    );
   }
 
   return $self->success;
@@ -90,3 +96,59 @@ sub send_email {
 __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Email::Sender::Transport::SQLite - deliver mail to an sqlite db for testing
+
+=head1 VERSION
+
+version 0.091660
+
+=head1 DESCRIPTION
+
+This transport makes deliveries to an SQLite database, creating it if needed.
+The SQLite transport is intended for testing programs that fork or that
+otherwise can't use the Test transport.  It is not meant for robust, long-term
+storage of mail.
+
+The database will be created in the file named by the C<db_file> attribute,
+which defaults to F<email.db>.
+
+The database will have two tables:
+
+  CREATE TABLE emails (
+    id INTEGER PRIMARY KEY,
+    body     varchar NOT NULL,
+    env_from varchar NOT NULL
+  );
+
+  CREATE TABLE recipients (
+    id INTEGER PRIMARY KEY,
+    email_id integer NOT NULL,
+    env_to   varchar NOT NULL
+  );
+
+Each delivery will insert one row to the F<emails> table and one row per
+recipient to the F<recipients> table.
+
+Delivery to this transport should never fail.
+
+=head1 AUTHOR
+
+  Ricardo Signes <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Ricardo Signes.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut 
+
+
